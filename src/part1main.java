44cb4eafbd60c5f63ce9fb794df5408b8581a597
lxgai <lgai@ucsd.edu>
2021-01-28 16:34:42 -0800

    allow for doubles instead of just ints in matrices

--- src/part1main.java
+++ src/part1main.java
@@ -1,3 +1,6 @@
+// Lucy Gai
+// PA1 CSE 151A
+
 import java.io.*;
 import java.util.*;
 import java.lang.*;
@@ -5,8 +8,8 @@ import java.lang.*;
 public class part1main {
     public static void main(String[] args) {
 
-        // list of training data
-        ArrayList<Pair<int[], Integer>> trainlist = new ArrayList<Pair<int[], Integer>>();
+        // list of training data (vector, label)
+        ArrayList<Pair<double[], Integer>> trainlist = new ArrayList<Pair<double[], Integer>>();
 
         // other elements
         int kval = 0;
@@ -32,23 +35,18 @@ public class part1main {
                 // obtain data (vector) point
                 String line = dataRead.nextLine();
                 String[] separate = line.split(" ");
-                int[] datavec = new int[separate.length - 1];
+                double[] datavec = new double[separate.length - 1];
                 for (int i = 0; i < separate.length - 1; ++i) {
-                    datavec[i] = Integer.parseInt(separate[i]);
+                    datavec[i] = Double.parseDouble(separate[i]);
                 }
 
                 // obtain label
-                int tmp = Integer.parseInt(separate[separate.length - 1]);
+                int tmp = (int)Double.parseDouble(separate[separate.length - 1]);
 
                 // put (vector, label) pair into list
-                trainlist.add(new Pair<int[], Integer>(datavec, tmp));
+                trainlist.add(new Pair<double[], Integer>(datavec, tmp));
 
-                // PRINT TRAINING DATA
-                //System.out.println("Training data: ");
-                //for (int i = 0; i < trainlist.size(); i++) {
-                   // System.out.println("Vector: " + trainlist.get(i).getKey() + " -- Label: " + trainlist.get(i).getValue() );
-
-                //}
+              
             }
 
             dataRead.close();
@@ -77,15 +75,15 @@ public class part1main {
                     // obtain a data (vector) point from the test data
                     String line = testRead.nextLine();
                     String[] separate = line.split(" ");
-                    int[] datavec = new int[separate.length - 1];
+                    double[] datavec = new double[separate.length - 1];
                     for (int i = 0; i < separate.length - 1; ++i) {
-                        datavec[i] = Integer.parseInt(separate[i]);
+                        datavec[i] = Double.parseDouble(separate[i]);
                     }
 
                     
 
                     // the correct label for the data (vector) point
-                    int truLabel = Integer.parseInt(separate[separate.length - 1]);
+                    int truLabel = (int)Double.parseDouble(separate[separate.length - 1]);
 
                     
 
@@ -118,7 +116,7 @@ public class part1main {
 
     }
 
-    public static int predict(int[] vec, ArrayList<Pair<int[],Integer>> trainlist, int kval) {
+    public static int predict(double[] vec, ArrayList<Pair<double[],Integer>> trainlist, int kval) {
         
         // store (dist, label) pairs 
         ArrayList<Pair<Double,Integer>> distResults = new ArrayList<Pair<Double,Integer>>();
@@ -136,6 +134,9 @@ public class part1main {
     public static int findMajority(ArrayList<Pair<Double,Integer>> distResults, int kval) {
         // to update dist counts and get majority
         int[] arr = new int[10];
+
+        // shuffle first for random order so we don't have to worry about breaking ties
+        Collections.shuffle(distResults);
 
         // sort the (dist, label) pairs by increasing distance
         Collections.sort(distResults, new Comparator<Pair<Double, Integer>>() {
@@ -175,7 +176,7 @@ public class part1main {
         return majlabel;
     }
 
-    public static double dist(int[] one, int[] two) {
+    public static double dist(double[] one, double[] two) {
         double sum = 0;
         
         for (int i = 0; i < one.length; ++i) {
@@ -183,25 +184,6 @@ public class part1main {
         }
         return Math.sqrt(sum);
     }
-
-    // read in training data and put it into an array?? or other data structure
-    // read in value of k
-    /*
-     * method to predict one test data label (one line): 
-     *      find distance between test point and all points in training array 
-     *      use dist method in a loop & store (dist, label) pairs in result array 
-     *      find the k lowest distances return the
-     * majority label of the k lowest distances findmajority method
-     * https://www.geeksforgeeks.org/sort-an-array-of-pairs-using-java-pair-and-comparator/ (resolve ties randomly)
-     * 
-     * ------
-     * 
-     * compare this output with the output in the training array for this data point
-     * add 1 to error count if wrong add 1 to overall count every time (unless we
-     * already have overall count)
-     * 
-     */
-
 
 
     static class Pair<K, V> {
